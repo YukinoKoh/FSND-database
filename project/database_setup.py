b5746fcad60565f07a7c3fcf752ac9a96c90e053
YukinoKohmoto <yukino@YukinoKohmotos-MacBook-Pro.local>
2017-11-05 00:37:48 +0900

    Added project

--- project/database_setup.py
+++ project/database_setup.py
@@ -8,24 +8,31 @@ from sqlalchemy import create_engine
 # instance
 Base = declarative_base()
 
+
+# user
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+
+
 # mapper
 class Category(Base):
-    __tablename__='categories'
+    __tablename__ = 'categories'
     id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     name = Column(String(80), nullable=False)
     description = Column(String(250), nullable=False)
 
 
-class Tool(Base):
-    __tablename__='tools'
+class Option(Base):
+    __tablename__ = 'options'
     id = Column(Integer, primary_key=True)
     cat_id = Column(Integer, ForeignKey('categories.id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     name = Column(String(80), nullable=False)
     description = Column(String(250), nullable=False)
-    image = Column(String(80))
-    who = Column(String(250))
-    preparation = Column(String(250))
-    question = Column(String(250))
 
     @property
     def serialize(self):
@@ -34,21 +41,19 @@ class Tool(Base):
             'name': self.name,
             'description': self.description,
             'id': self.id,
-            'cat_id':self.cat_id
+            'cat_id': self.cat_id
         }
 
+
 class Link(Base):
-    __tablename__='links'
+    __tablename__ = 'links'
     id = Column(Integer, primary_key=True)
-    tool_id = Column(Integer, ForeignKey('tools.id'))
+    option_id = Column(Integer, ForeignKey('options.id'))
     title = Column(String(80), nullable=False)
     url = Column(String(250), nullable=False)
-    source = Column(String(80))
 
 
-
-engine = create_engine(
-'sqlite:///researchtool.db')
+engine = create_engine('sqlite:///researchoption.db')
 
 Base.metadata.create_all(engine) 
 
--- project/project.py
+++ project/project.py
@@ -1,119 +1,189 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Category, Tool, Link
-
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import jsonify
+from database_setup import Category, Option, Link
+from flask import session as login_session
+from signin import doSignout, fbConnect, returnUserId, signin_required
+from common import session, meta_title, meta_desc, meta_url
+from common import get_cats, get_cat_by_name, get_options_by_cat
+from common import get_option_by_id, get_option_by_name, update_option
+from common import get_links_by_opId, get_link_by_id
 # app name
 app = Flask(__name__)
 
-# sqlalchemy to access db
-engine = create_engine('sqlite:///researchtool.db')
-Base.metadata.bind = engine
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
 
-# common info 
-meta_title = ' Design Research'
-meta_desc = 'Design research resources and links to keep interests in projects.'
-meta_url = 'URL_MAIN'
-
-
-# flask's decoration
-# '/' will run hello function
 @app.route('/')
 def main():
-    # render tempaltes, using Flask function
-    categories = session.query(Category).all()
-    return render_template('main.html', meta_title=meta_title, meta_desc=meta_desc, categories=categories)
+    log = str(login_session.has_key('username'))
+    return render_template('main.html', meta_title=meta_title,
+                           meta_desc=meta_desc,
+                           log=log, categories=get_cats())
 
 
-# category
+@app.route('/signin')
+def signin():
+    log = str(login_session.has_key('username'))
+    return render_template('signin.html', meta_title=meta_title,
+                           meta_desc=meta_desc, log=log,
+                           categories=get_cats())
+
+
+@app.route('/signout')
+def signout():
+    global log
+    log = doSignout()
+    return redirect(url_for('main'))
+
+
+@app.route('/fbsignin', methods=['POST'])
+def fbsignin():
+    global log
+    log = fbConnect()
+    return 'Success'
+
+
+# caegory
 @app.route('/<string:category_name>')
 def category(category_name):
-    categories = session.query(Category).all()
-    category_name = category_name.title()
-    category = session.query(Category).filter_by(name=category_name).one()
-    cat_id = category.id
-    tools = session.query(Tool).filter_by(cat_id=cat_id).all()
+    log = str(login_session.has_key('username'))
+    category = get_cat_by_name(category_name)
+    options = get_options_by_cat(category)
+    return render_template('category.html', meta_title=meta_title,
+                           meta_desc=meta_desc, log=log,
+                           categories=get_cats(), category=category,
+                           options=options)
 
-    tool_id_list =[]
-    for i in tools:
-        tool_id_list.append(i.id)
-    
-    links = None;
-    for i in tool_id_list:
-        q1 = session.query(Link).filter_by(tool_id=i).all()
-        if links is None:
-            links = q1
+
+# newOption
+@app.route('/<string:category_name>/new', methods=['GET', 'POST'])
+@signin_required
+def newOption(category_name):
+    log = str(login_session.has_key('username'))
+    category = get_cat_by_name(category_name)
+    if request.method == 'POST':
+        # check if required fields are filled
+        if request.form['name'] == '' or request.form['description'] == '':
+            warning = "Fill both name and description to add  option"
+            return render_template('new-option.html', category=category,
+                                   categories=get_cats(), log=log,
+                                   warning=warning)
+        # create new option
         else:
-            links = links.append(q1)
-
-    return render_template('category.html', meta_title=meta_title, meta_desc=meta_desc,
-                           categories=categories, category=category, tools=tools, links=links)
-
-# return link list
-def get_link(tool_id):
-    links = session.query(Link).filter_by(toold_id=tool_id).all()
-    return links
-
-# new
-@app.route('/restaurants/<int:restaurant_id>/new', methods=['GET', 'POST'])
-def newMenuItem(restaurant_id):
-    if request.method == 'POST':
-        newItem = MenuItem(
-            name=request.form['name'], restaurant_id=restaurant_id)
-        session.add(newItem)
-        session.commit()
-        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
-    else: 
-        return render_template('new.html', restaurant_id=restaurant_id)
-
-
-# edit
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit', methods=['GET', 'POST'])
-def editMenuItem(restaurant_id, menu_id):
-    editedItem = session.query(MenuItem).filter_by(id=menu_id).one()
-    if request.method == 'POST':
-        if request.form['name']:
-            editedItem.name = request.form['name']
-        session.add(editedItem)
-        session.commit()
-        flash("new menu item has created.")
-        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+            user_id = login_session['user_id']
+            anotherOption = Option(name=request.form['name'],
+                                   description=request.form['description'],
+                                   cat_id=category.id, user_id=user_id)
+            session.add(anotherOption)
+            session.commit()
+            return redirect(url_for('category', category_name=category.name))
     else:
-        return render_template('edit.html', restaurant_id=restaurant_id, menu_id=menu_id, item=editedItem)
+        return render_template('new-option.html', category=category, categories=get_cats(), log=log)
 
 
-# delete
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete', methods=['GET', 'POST'])
-def deleteMenuItem(restaurant_id, menu_id):
-    deleteItem = session.query(MenuItem).filter_by(id=menu_id).one()
-    if request.method == 'POST':
-        session.delete(deleteItem)
-        session.commit()
-        flash("The menu item has deleted.")
-        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+# caegory
+@app.route('/<string:category_name>/<int:option_id>')
+def option(category_name, option_id):
+    log = str(login_session.has_key('username'))
+    category = get_cat_by_name(category_name)
+    option = get_option_by_id(option_id)
+    links = get_links_by_opId(option_id)
+    return render_template('option.html', meta_title=meta_title,
+                           meta_desc=meta_desc, log=log,
+                           categories=get_cats(), category=category,
+                           option=option, links=links)
+
+
+@signin_required
+@app.route('/<string:category_name>/<int:option_id>/edit',
+           methods=['GET', 'POST'])
+def editOption(category_name, option_id):
+    log = str(login_session.has_key('username'))
+    user_id = login_session['user_id']
+    category = get_cat_by_name(category_name)
+    option = get_option_by_id(option_id)
+    links = get_links_by_opId(option_id)
+    # check if obj in db
+    if category and option:
+        if request.method == 'POST':
+            f = request.form
+            if f['name'] == '' or f['description'] == '':
+                warning = "You need both Research Option' and 'Description'"
+                return render_template('edit-option.html', log=log,
+                                       meta_title=meta_title,
+                                       meta_desc=meta_desc,
+                                       categories=get_cats(),
+                                       category=category,
+                                       option=option, links=links,
+                                       warning=warning)
+            else:
+                update_option(option, f)
+                category = session.query(Category).filter_by(id=option.cat_id).one()
+                flash(option.name+' has updated')
+                return redirect(url_for('option', category_name=category.name,
+                                option_id=option.id))
+        # show option page
+        else:
+            return render_template('edit-option.html', meta_title=meta_title,
+                                   meta_desc=meta_desc, log=log,
+                                   categories=get_cats(), category=category,
+                                   option=option, links=links)
+
+
+@signin_required
+@app.route('/<string:category_name>/<int:option_id>/newlink', methods=['POST'])
+def addLink(category_name, option_id):
+    log = str(login_session.has_key('username'))
+    user_id = login_session['user_id']
+    option = get_option_by_id(option_id)
+    f = request.form
+    if f['link'] == '' or f['url'] == '':
+        flash('You need both Link name and Url to add a new link')
     else:
-        return render_template('delete.html', item=deleteItem)
+        link = Link(title=f['link'], url=f['url'], option_id=option.id)
+        session.add(link)
+        session.commit()
+    return redirect(url_for('editOption', category_name=category_name,
+                            option_id=option_id))
+
+
+@signin_required
+@app.route('/<string:category_name>/<int:option_id>/delete')
+def deleteOption(category_name, option_id):
+    option = get_option_by_id(option_id)
+    delete_name = option.name
+    session.delete(option)
+    session.commit()
+    flash(delete_name + ' has been deleted.')
+    return redirect(url_for('category', category_name=category_name))
+
+
+@signin_required
+@app.route('/<string:category_name>/<int:option_id>/<int:link_id>/delete')
+def deleteLink(category_name, option_id, link_id):
+    link = get_link_by_id(link_id)
+    delete_name = link.title
+    session.delete(link)
+    session.commit()
+    flash(delete_name + ' has been deleted.')
+    return redirect(url_for('editOption', category_name=category_name,
+                            option_id=option_id))
 
 
 # return JSON
-@app.route('/restaurants/<int:restaurant_id>/menu/JSON')
-def restaurantMenuJSON(restaurant_id):
-    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
-    items = session.query(MenuItem).filter_by(
-        restaurant_id=restaurant_id).all()
-    return jsonify(MenuItems=[i.serialize for i in items])
+@app.route('/<string:category_name>/JSON')
+def categoryJSON(category_name):
+    category = get_cat_by_name(category_name)
+    options = get_options_by_cat(category)
+    return jsonify(Options=[i.serialize for i in options])
 
 
-@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/JSON')
-def menuItemJSON(restaurant_id, menu_id):
-    menuItem = session.query(MenuItem).filter_by(id=menu_id).one()
-    return jsonify(MenuItem=menuItem.serialize)
+@app.route('/<string:category_name>/<int:option_id>/JSON')
+def optionJSON(category_name, option_id):
+    option = get_option_by_id(option_id)
+    return jsonify(Option=option.serialize)
 
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
     # let hear any webserver
-    app.run(host='0.0.0.0', port = 5000)
+    app.run(host='0.0.0.0', port=5000)
